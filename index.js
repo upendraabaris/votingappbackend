ac642b21a1e43bc5dd8fe4386f09499550e0e33b
upendraabaris <upendra.abaris@gmail.com>
2024-11-22 23:17:52 +0530

    fix cors issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-
+import cors from "cors";
 const app = express()
 
 dotenv.config();
