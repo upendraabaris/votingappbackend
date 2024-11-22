500871309526b90c87daf87a471a32aa5807b466
upendraabaris <upendra.abaris@gmail.com>
2024-11-22 23:24:34 +0530

    fix cors issue

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ mongoose.connect(process.env.MONGODB_URL).then(()=>{
 app.use(bodyParser.json({limit:"50mb"}));
 app.use(bodyParser.urlencoded({extended:false, limit:"50mb"}));
 
-// Use CORS middleware
+// Use the CORS middleware
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:5173', // Frontend's origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+    credentials: true, // If you are using cookies or authorization headers
 }));
+
 
 
 // Import Routes
